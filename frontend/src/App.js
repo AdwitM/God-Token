f8892776023d833715ac40006e383d831861a7d1
Adwit Mittal <adwitmittal@gmail.com>
2022-07-25 16:28:54 +0530

    logo update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -152,9 +152,8 @@ function App() {
   return (
     <main className="main-container">
       <h2 className="headline">
-        <span className="headline-gradient">GOD TOKEN 🧔</span>
+        <span className="headline-gradient">GOD TOKEN</span>
         <span className="inline p-3 ml-2">🧔</span>
-        {/* <img className="inline p-3 ml-2" src="https://i.imgur.com/WKHHO8R.png" alt="GOD TOKEN" width="60" height="30" /> */}
       </h2>
       <section className="customer-section px-10 pt-5 pb-10">
         {error && <p className="text-2xl text-red-700">{error}</p>}
